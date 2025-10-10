dfe16a9350e79025799fcba7ff411c466ab7a2f3
胡闰智 <hurunzhi@192.168.1.56>
2025-10-10 16:58:07 +0800

    fix setup version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from pathlib import Path
 this_directory = Path(__file__).parent
 setup(
     name="tasgnss",
-    version="0.1.3",
+    version="0.1.4",
     author="Runzhi Hu",
     author_email="run-zhi.hu@connect.polyu.hk",
     description="A Python package for GNSS positioning and processing by TASLAB",
